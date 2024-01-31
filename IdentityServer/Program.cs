7f52264cf45f6e2d5e81a95d36b84ee1c345924c
dwkim <kimdaewoong@kakao.com>
2024-01-31 13:29:15 +0900

    10. 클라이언트 리소스 범위, 테스트 사용자 구성  // IdentityServer4 종속성 주입  builder.Services.AddIdentityServer()       .AddInMemoryClients(new List<Client>())       .AddInMemoryApiScopes(new List<ApiScope>())       .AddInMemoryIdentityResources(new List<IdentityResource>())       .AddTestUsers(new List<TestUser>())       .AddDeveloperSigningCredential();

--- IdentityServer/Program.cs
+++ IdentityServer/Program.cs
@@ -4,9 +4,6 @@ using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
-
 // IdentityServer4 종속성 주입
 //builder.Services.AddIdentityServer();
 builder.Services.AddIdentityServer()
@@ -16,27 +13,12 @@ builder.Services.AddIdentityServer()
     .AddTestUsers(new List<TestUser>())
     .AddDeveloperSigningCredential();
 
-
-
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
 
 app.UseRouting();
 // IdentityServer4 미들웨어 추가
-app.UseAuthentication();
+app.UseIdentityServer();
 
-app.UseAuthorization();
-
-app.MapRazorPages();
+app.MapGet("/", () => "Hello World!");
 
 app.Run();
