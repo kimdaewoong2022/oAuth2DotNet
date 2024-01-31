7a905cc83694f13958581ef196dd3467245e448d
dwkim <kimdaewoong@kakao.com>
2024-01-31 11:17:20 +0900

    8. X IdentityServer4 삭제

--- IdentityServer/Program.cs
+++ IdentityServer/Program.cs
@@ -1,10 +1,22 @@
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 
 // IdentityServer4 종속성 주입
-builder.Services.AddIdentityServer();
+//builder.Services.AddIdentityServer();
+builder.Services.AddIdentityServer()
+    .AddInMemoryClients(new List<Client>())
+    .AddInMemoryApiScopes(new List<ApiScope>())
+    .AddInMemoryIdentityResources(new List<IdentityResource>())
+    .AddTestUsers(new List<TestUser>())
+    .AddDeveloperSigningCredential();
+
+
 
 var app = builder.Build();
 
