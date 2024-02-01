38e28cefc5961ccefbc9c15fcaa8cffcdc6abe0a
dwkim <kimdaewoong@kakao.com>
2024-02-01 13:54:22 +0900

    5-3. MoviesControllern 추가  - Entity Framework를 사용, 뷰가 포함된 MVC 컨트롤러 추가  - Movie 상단 메뉴 추가

--- Movies.Client/Program.cs
+++ Movies.Client/Program.cs
@@ -1,4 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Movies.Client.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<MoviesClientContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MoviesClientContext") ?? throw new InvalidOperationException("Connection string 'MoviesClientContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
