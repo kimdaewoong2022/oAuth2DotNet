f11ca75987446f1366c4fdeda940f4055e24e0a2
dwkim <kimdaewoong@kakao.com>
2024-02-02 15:19:30 +0900

    8-2 보호된 Movies API 리소스 가져오기 1. API 자격 증명을 "검색"합니다. 이는 Identity Server에 등록되어야 합니다! 2. Identity Server에서 액세스 토큰을 인증하고 가져옵니다. 3. 요청에 access_token을 설정합니다. Authorization: Bearer <token> 4. 보호된 API에 요청 보내기

--- Movies.Client/ApiServices/MovieApiService.cs
+++ Movies.Client/ApiServices/MovieApiService.cs
@@ -1,4 +1,6 @@
-﻿using Movies.Client.Models;
+﻿using IdentityModel.Client;
+using Movies.Client.Models;
+using Newtonsoft.Json;
 
 namespace Movies.Client.ApiServices
 {
@@ -6,20 +8,67 @@ namespace Movies.Client.ApiServices
     {
         public async Task<IEnumerable<Movie>> GetMovies()
         {
-            var movieList = new List<Movie>();
-            movieList.Add(new Movie
-            {
-                Id = 1,
-                Genre = "Comic",
-                Title = "10배의 법칙",
-                ImageUrl = "images/src",
-                ReleaseDate = DateTime.Now,
-                Owner = "Kim",
-                Rating = "9.9"
-            }
-            );
+            //임시 Data 
+            //var movieList = new List<Movie>();
+            //movieList.Add(new Movie
+            //{
+            //    Id = 1,
+            //    Genre = "Comic",
+            //    Title = "10배의 법칙",
+            //    ImageUrl = "images/src",
+            //    ReleaseDate = DateTime.Now,
+            //    Owner = "Kim",
+            //    Rating = "9.9"
+            //}
+            //);
 
-            return await Task.FromResult( movieList );
+            //return await Task.FromResult( movieList );
+                    
+
+            // 1. "retrieve" our api credentials. This must be registered on Identity Server!
+            // 1. API 자격 증명을 "retrieve"합니다. 이는 Identity Server에 등록되어야 합니다!
+            var apiClientCredentials = new ClientCredentialsTokenRequest
+            {
+                Address = "https://localhost:5005/connect/token",
+
+                ClientId = "movieClient",
+                ClientSecret = "secret",
+
+                // This is the scope our Protected API requires. 
+                Scope = "movieAPI"
+            };
+           
+            // IdentityServer(localhost:5005)와 통신하기 위해 새로운 HttpClient를 생성합니다.
+            var client = new HttpClient();
+
+            // Discovery 문서에 접근할 수 있는지 확인합니다. 100% 필요하지는 않지만..
+            var disco = await client.GetDiscoveryDocumentAsync("https://localhost:5005");
+            if (disco.IsError)
+            {
+                return null; // throw 500 error
+            }
+
+            // 2. Identity Server에서 액세스 토큰을 인증하고 가져옵니다.            
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(apiClientCredentials);
+            if (tokenResponse.IsError)
+            {
+                return null;
+            }
+
+            // 이제 보호된 API와 통신하기 위한 또 다른 HttpClient
+            var apiClient = new HttpClient();
+
+            // 3. 요청에 access_token을 설정합니다. Authorization: Bearer <token>            
+            client.SetBearerToken(tokenResponse.AccessToken);
+
+            // 4. 보호된 API에 요청 보내기
+            var response = await client.GetAsync("https://localhost:5001/api/movies");
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            var movieList = JsonConvert.DeserializeObject<List<Movie>>(content);
+            return movieList;
         }
 
         public Task<Movie> GetMovie(string id)
