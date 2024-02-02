48c73d7d252af7797a20b256fae6410e7df743f9
dwkim <kimdaewoong@kakao.com>
2024-02-02 14:02:30 +0900

    7-3.  로그아웃 기능 추가

--- IdentityServer/Quickstart/Account/AccountOptions.cs
+++ IdentityServer/Quickstart/Account/AccountOptions.cs
@@ -13,6 +13,8 @@ namespace IdentityServerHost.Quickstart.UI
         public static TimeSpan RememberMeLoginDuration = TimeSpan.FromDays(30);
 
         public static bool ShowLogoutPrompt = true;
+
+        //로그아웃시 자동으로 리다이렉트를 할지 말지
         public static bool AutomaticRedirectAfterSignOut = false;
 
         public static string InvalidCredentialsErrorMessage = "Invalid username or password";
